58f968b61fe364da965251a79861b634ecde23b3
ArtemTodor <todorartem@icloud.com>
2023-07-25 12:54:04 +0300

    add nvim

---
+++ nvim/lua/myconf/init.lua
@@ -0,0 +1,5 @@
+require('myconf.remap')
+require('myconf.set')
+
+vim.o.background = "dark"
+vim.cmd([[colorscheme gruvbox]])
---
+++ nvim/lua/myconf/packer.lua
@@ -0,0 +1,36 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.2',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use { "ellisonleao/gruvbox.nvim" }
+	use ('nvim-treesitter/nvim-treesitter',
+	{run = ':TSUpdate'})
+	use ('theprimeagen/harpoon')
+	use ('mbbill/undotree')
+	use ('tpope/vim-fugitive')
+
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{                                      -- Optional
+			'williamboman/mason.nvim',
+			run = function()
+				pcall(vim.api.nvim_command, 'MasonUpdate')
+			end,
+		},
+		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+		-- Autocompletion
+		{'hrsh7th/nvim-cmp'},     -- Required
+		{'hrsh7th/cmp-nvim-lsp'}, -- Required
+		{'L3MON4D3/LuaSnip'},     -- Required
+	}
+}
+end)
